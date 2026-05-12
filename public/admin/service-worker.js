ddee792e30d5deabdd701428cf74e1ab7521c65c
Pengkypeng-sys <feriman.getup@gmail.com>
2026-05-12 14:08:19 +0700

    Update service worker to Network First strategy to bust stale cache

--- public/admin/service-worker.js
+++ public/admin/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quotepro-v1';
+const CACHE_NAME = 'quotepro-v2';
 const ASSETS = [
   'index.html',
   'style.css',
@@ -7,13 +7,31 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
+  // Network First Strategy
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
