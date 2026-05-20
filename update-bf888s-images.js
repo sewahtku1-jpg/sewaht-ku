ec266ca7c7933f05d607db4b478944c3ea2c8c55
Pengkypeng-sys <feriman.getup@gmail.com>
2026-05-20 12:10:57 +0700

    feat: replace BF-888S images with new ones

---
+++ update-bf888s-images.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  await prisma.item.update({
+    where: { id: 'I-001' },
+    data: { img: '/assets/bf888s_new_1.png,/assets/bf888s_new_2.png,/assets/bf888s_new_3.png' }
+  });
+  console.log('Images updated');
+}
+main().catch(e => console.error(e)).finally(() => prisma.$disconnect());
