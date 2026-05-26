6dcbaf6ce289c359c79205877a4bb69bf220f653
Pengkypeng-sys <feriman.getup@gmail.com>
2026-05-26 11:13:35 +0700

    fix: fallback to Vercel POSTGRES_URL if DATABASE_URL is not defined

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL;
 
 const prismaClientSingleton = () => {
   const pool = new Pool({ 
