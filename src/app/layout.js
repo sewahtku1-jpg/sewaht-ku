bd7c08efadf8730c86f5eb5cd43ced8932701f33
Pengkypeng-sys <feriman.getup@gmail.com>
2026-05-26 12:46:56 +0700

    style: remove Profesional and Full-Stack wording

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Sewa HT Ku - Full-Stack Rental HT Profesional",
+  title: "Sewa HT Ku - Rental HT",
   description: "Solusi komunikasi terbaik untuk segala jenis acara Anda. Sewa HT berkualitas, sinyal jernih, dan harga terjangkau.",
 };
 
